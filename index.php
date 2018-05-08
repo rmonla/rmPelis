e8dfc570a7632454eab1bb849d7e08d0b8f297ab
Ricardo N. MONLA <rmonla@gmail.com>
2018-05-08 10:26:32 -0300

    Agrego comentarios en el código.

--- index.php
+++ index.php
@@ -10,28 +10,34 @@
  */
 
 
-	
+/**
+ * Imprime en pantalla una <li> de archivos.
+ * @param  string $dori  Directorio origen de búsqueda.
+ * @param  array  $exts  Extensiones permitaidas a mostrar.
+ * @param  array  $outs  Archivos negados a mostrar.
+ * @return html          
+ */
 function lstPelis( 
-    $dorgen  = '.', 
-    $eactdas = array('mp4'), 
-    $angdos  = array('.', '..', '.htaccess')
+    $dori = '.', 
+    $exts = array('mp4'), 
+    $outs = array('.', '..', '.htaccess')
 ){
-    if( is_dir($dorgen) ){
-        if( $dir = opendir($dorgen) ){
+    if( is_dir($dori) ){
+        if( $dir = opendir($dori) ){
             while( ($arch = readdir($dir)) !== false ){
                 
-                if( !in_array(strtolower(end(explode('.', $arch))), $eactdas) ) continue;
+                if( !in_array(strtolower(end(explode('.', $arch))), $exts) ) continue;
                 // explode('.', xxx)        --> Divide xxx en array por el punto.
                 // end( xxx )               --> Toma el último elemento del array.
                 // strtolower ( xxx )       --> Pasa a minúsculas xxx.
                 // !in_array( xxx , array ) --> Verifica si xxx NO está en el array.
                 // if( xxx ) continue       --> Salta y sigue el while.
                 
-                if( in_array($arch, $angdos) ) continue;
+                if( in_array($arch, $outs) ) continue;
                 // in_array( xxx , array )  --> Verifica si xxx ESTA en el array.
                 // if( xxx ) continue       --> Salta y sigue el while.
 
-                echo '<li><a target="_blank" href="'.$dorgen.'/'.$arch.'">'.$arch.'</a></li>';
+                echo '<li><a target="_blank" href="'.$dori.'/'.$arch.'">'.$arch.'</a></li>';
             }
             closedir($dir);
         }
@@ -39,29 +45,10 @@ function lstPelis(
 }
  
 echo lstPelis();
-
-/**
- * Retorna un array de archivos.
- * @param  string $pth Path del directorio donde se hace la búsqueda de archivos.
- * @return array       Lista de archivos encontrados.
- */
-// function getLstArchs($pth='.'){
-// 	$lst = [];
-// 	while ( $arch = readdir($pth) )
-// 	    if ( !is_dir($arch) ){
-// 	    	$lst[] = $arch;
-// 	    	echo "$arch <br>";
-// 	    }
-//     return $lst;
-// }
-
-// var_dump( getLstArchs() );
-
-
-
 ?>
 
-<video width="320" height="240" controls>
+<!-- <video width="320" height="240" controls>
 	<source src="Peli_CARS33333HD720P.mp4" type="video/mp4">
 	Your browser does not support the video tag.
 </video> 
+ -->
