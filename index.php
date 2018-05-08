495b38c749dfd8f52db411d12dbe1738c582f091
Ricardo N. MONLA <rmonla@gmail.com>
2018-05-08 09:04:38 -0300

    Agrega array de extensiones permitidas y array de archivos negados.

--- index.php
+++ index.php
@@ -8,15 +8,19 @@
  */
 
 	
-function lstPelis($dori='.'){
-    if(is_dir($dori)){
-        if($d = opendir($dori)){
-            while(($arch = readdir($d)) !== false){
-                if($arch != '.' && $arch != '..' && $arch != '.htaccess'){
-                    echo '<li><a target="_blank" href="'.$dori.'/'.$arch.'">'.$arch.'</a></li>';
-                }
+function lstPelis(
+    $dorgen  = '.',
+    $eactdas = ['.mp4'],
+    $angdos  = ['.', '..', '.htaccess']
+){
+    if( is_dir($dorgen) ){
+        if( $dir = opendir($dorgen) ){
+            while( ($arch = readdir($dir)) !== false ){
+                if( in_array($arch, $angdos) ) continue;
+
+                echo '<li><a target="_blank" href="'.$dorgen.'/'.$arch.'">'.$arch.'</a></li>';
             }
-            closedir($d);
+            closedir($dir);
         }
     }
 }
@@ -28,15 +32,15 @@ echo lstPelis();
  * @param  string $pth Path del directorio donde se hace la búsqueda de archivos.
  * @return array       Lista de archivos encontrados.
  */
-function getLstArchs($pth='.'){
-	$lst = [];
-	while ( $arch = readdir($pth) )
-	    if ( !is_dir($arch) ){
-	    	$lst[] = $arch;
-	    	echo "$arch <br>";
-	    }
-    return $lst;
-}
+// function getLstArchs($pth='.'){
+// 	$lst = [];
+// 	while ( $arch = readdir($pth) )
+// 	    if ( !is_dir($arch) ){
+// 	    	$lst[] = $arch;
+// 	    	echo "$arch <br>";
+// 	    }
+//     return $lst;
+// }
 
 // var_dump( getLstArchs() );
 
