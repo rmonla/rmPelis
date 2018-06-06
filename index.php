a48aab60aa08aef1713f2e38e709e21a1ed5588e
rmonla <rmonla@gmail.com>
2018-06-06 09:53:30 -0300

    Se agregan nueva funciones y se actualiza el .gitignore

--- index.php
+++ index.php
@@ -25,30 +25,70 @@ function lstPelis(
     if( is_dir($dori) ){
         if( $dir = opendir($dori) ){
             while( ($arch = readdir($dir)) !== false ){
-                
                 if( !in_array(strtolower(end(explode('.', $arch))), $exts) ) continue;
-                // explode('.', xxx)        --> Divide xxx en array por el punto.
-                // end( xxx )               --> Toma el último elemento del array.
-                // strtolower ( xxx )       --> Pasa a minúsculas xxx.
-                // !in_array( xxx , array ) --> Verifica si xxx NO está en el array.
-                // if( xxx ) continue       --> Salta y sigue el while.
-                
-                if( in_array($arch, $outs) ) continue;
-                // in_array( xxx , array )  --> Verifica si xxx ESTA en el array.
-                // if( xxx ) continue       --> Salta y sigue el while.
-
+                    // Salta si la extensión esta en la lista de las negadas.
+                if( in_array($arch, $outs) ) continue; 
+                    // Salta si el archivo está en la lista de los negados.
                 echo '<li><a target="_blank" href="'.$dori.'/'.$arch.'">'.$arch.'</a></li>';
             }
             closedir($dir);
         }
     }
 }
- 
-echo lstPelis();
-?>
+/**
+ * Imprime en pantalla una <li> de archivos.
+ * @param  string $dori  Directorio origen de búsqueda.
+ * @param  array  $exts  Extensiones permitaidas a mostrar.
+ * @param  array  $outs  Archivos negados a mostrar.
+ * @return html          
+ */
+function lstArchs( 
+    $dori = '.', 
+    $exts = array('mp4'), 
+    $outs = array('.', '..', '.htaccess')
+){
+    $lst = array();
+    if( is_dir($dori) ){
+        if( $dir = opendir($dori) ){
+            while( ($arch = readdir($dir)) !== false ){
+                if( !in_array(strtolower(end(explode('.', $arch))), $exts) ) continue;
+                    // Salta si la extensión NO está en la lista de permitidas.
+                if( in_array($arch, $outs) ) continue; 
+                    // Salta si el archivo está en la lista de los negados.
+                $lst[] = $arch;
+                    //Carga en el array el archivo.
+            }
+            closedir($dir);
+        }
+    }
+    if ( count($lst) ) sort($lst);
+    else return 0;
+}
 
-<!-- <video width="320" height="240" controls>
-	<source src="Peli_CARS33333HD720P.mp4" type="video/mp4">
-	Your browser does not support the video tag.
-</video> 
- -->
+function lstDirs( 
+    $dori = '.', 
+    $outs = array()
+){
+    $lst = array();
+    if( is_dir($dori) ){
+        if( $dir = opendir($dori) ){
+            while( ($dir = readdir($dir)) !== false ){
+                if( !is_dir($dori) ) continue;
+                    // Salta si no es directorio.
+                if( in_array($dir, $outs) ) continue; 
+                    // Salta si el directorio está en la lista de los negados.
+                $lst[] = $dir;
+                    //Carga en el array el directorio.
+            }
+            closedir($dir);
+        }
+    }
+    if ( count($lst) ) sort($lst);
+    else return 0;
+}
+
+echo "<p><h1><center>rmPelis</center></h1></p>";
+
+echo lstPelis();
+
+?>
