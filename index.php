e2aab964263bf7bdb43578a2654d90dd3049fa11
rmonla <rmonla@gmail.com>
2018-10-01 13:50:29 -0300

    V 1.0.2  *     - Se limpió código.  *     - Genera los links a partir de un array y no solo imprime como antes.

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 /**
  * Terminados:
  *     - Mostrar solo los archivos de una lista de extenciones pedidas.
+ * V 1.0.2
+ *     - Se limpió código.
+ *     - Genera los links a partir de un array y no solo imprime como antes.
+ *     
  * Pendientes:
  *     - Comentar mejor.
  *     - Hacer un mejor copyrigth.
@@ -18,36 +22,10 @@
  * @param  array  $outs  Archivos negados a mostrar.
  * @return html          
  */
-function lstPelis( 
-    $dori = '.', 
-    $exts = array('mp4'), 
-    $outs = array('.', '..', '.htaccess')
-){
-    if( is_dir($dori) ){
-        if( $dir = opendir($dori) ){
-            while( ($arch = readdir($dir)) !== false ){
-                if( !in_array(strtolower(end(explode('.', $arch))), $exts) ) continue;
-                    // Salta si la extensión esta en la lista de las negadas.
-                if( in_array($arch, $outs) ) continue; 
-                    // Salta si el archivo está en la lista de los negados.
-                echo '<li><a target="_blank" href="'.$dori.'/'.$arch.'">'.$arch.'</a></li>';
-            }
-            closedir($dir);
-        }
-    }
-}
-/**
- * Imprime en pantalla una <li> de archivos.
- * @param  string $dori  Directorio origen de búsqueda.
- * @param  array  $exts  Extensiones permitaidas a mostrar.
- * @param  array  $outs  Archivos negados a mostrar.
- * @return html          
- */
-function lstArchs( 
-    $dori = '.', 
-    $exts = array('mp4'), 
-    $outs = array('.', '..', '.htaccess')
-){
+function lstArchs( $dori = '.' ){
+    $exts = array('mp4'); 
+    $outs = array('.', '..', '.htaccess');
+
     $lst = array();
     if( is_dir($dori) ){
         if( $dir = opendir($dori) ){
@@ -81,7 +59,6 @@ function lstDirs(
                     // Salta si el directorio está en la lista de los negados.
                 if( is_dir($res) ) $lst[] = $res;
                     // Carga en el array si es directorio.
-                
             }
             closedir($dir);
         }
@@ -93,32 +70,27 @@ function lstDirs(
     else return 0;
 }
 
-function getLi(
-    $dir = '',
-    $arch = ''
-){
-    return "<li><a target='_blank' href='$dir/$arch'>$arch</a></li>";
+function getLi( $dir = '', $arch = ''){
+    $tit = substr($arch, 0, -4);
+    return "<li><a target='_blank' href='$dir/$arch'>$tit</a></li>";
 }
 
-$dirs = lstDirs();
-foreach ($dirs as $dir) {
-    echo "<li>$dir</li>";
-    $archs = lstArchs($dir, array('mp4'), array('.', '..'));
-    echo "<ul>";
-    foreach ($archs as $arch) echo getLi( $dir, $arch );
-    echo "</ul>";
+// Cargo los archivos en un array..
 
-}
+    foreach ( lstDirs() as $dir )
+        foreach ( lstArchs($dir) as $arch )
+            $pelis[$dir][] = $arch; 
+            
+// var_dump($pelis);
 
-// is_dir(filename)
-// Arma los ul
-//var_dump($dirs);
-// echo "<pre>";
-// var_dump($archs);
-// echo "</pre>";
+// Los links.
+    foreach ($pelis as $cat => $archs) {
+        echo "<ul><li>$cat</li><ul>";
+        
+        foreach ($archs as $arch) echo getLi( $cat, $arch );
+        
+        echo "</ul></ul>";
 
-// echo "<p><h1><center>rmPelis</center></h1></p>";
-
-// echo lstPelis();
+    }
 
 ?>
